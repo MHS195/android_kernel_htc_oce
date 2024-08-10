59a774fc30f621a88fe0e093a533250bff30fde8
MHS195 <m.hamdisharaf@gmail.com>
2024-08-10 17:04:35 +0000

    debian packaging

---
+++ debian/linux-headers-3.18-140-htc-oce/usr/src/linux-headers-3.18-140-htc-oce/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Aug 10 15:15:51 UTC 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "62c684dc8e0f"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
